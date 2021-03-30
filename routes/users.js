8fbf05f3592a98248cee8a5c9a653e9e438eae41
angel <angelbandera0@gmail.com>
2021-03-30 18:42:58 +0200

    valiacion de las rutas

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const { userPost, userGet, userPut,userDelete } = require("../controllers/userCo
 /* Route users listing. */
 router.get("/",userGet);
 router.post("/",[check("email","No es valido").isEmail(),validarCampos], userPost);
-router.put("/:id", userPut);
+router.put("/:id",[check("email","No es valido").isEmail(),validarCampos], userPut);
 router.delete("/", userDelete);
 
 module.exports = router;
