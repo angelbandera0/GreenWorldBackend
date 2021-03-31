7bb8b8ba2950f2777b452cf67de861f12fb95773
angel <angelbandera0@gmail.com>
2021-03-31 21:55:43 +0200

    Variable de entorno

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect( process.env.MONGODB_ATLAS_URL, {
+        await mongoose.connect( process.env.MONDODB_ATLAS_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
