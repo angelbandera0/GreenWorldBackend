71fa7316a31fb342719740b03b1ca38b687b3a0f
angel <angelbandera0@gmail.com>
2021-03-31 20:11:43 +0200

    Se agregaron las validaciones de los campos en las rutas

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
 var express = require("express");
 const {check}=require("express-validator");
-const {validarCampos} = require('../middlewares/validator');
 var router = express.Router();
+
+const {validarCampos} = require('../middlewares/validator');
+const { isRoleValid, emailExist, existUserById } = require('../helpers/users_db_validator');
+
 const { userPost, userGet, userPut,userDelete } = require("../controllers/userController");
 
 /* Route users listing. */
 router.get("/",userGet);
-router.post("/",[check("email","No es valido").isEmail(),validarCampos], userPost);
-router.put("/:id",[check("email","No es valido").isEmail(),validarCampos], userPut);
-router.delete("/:id", userDelete);
+
+router.post("/",[
+    check('name', 'El nombre es obligatorio').not().isEmpty(),    
+    check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
+    check("email","El correo no es válido").isEmail(),
+    check('email').custom( emailExist ),  
+    check('rol').custom( isRoleValid ), 
+    validarCampos], userPost);
+
+router.put("/:id",[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existUserById ),
+    check('rol').custom( isRoleValid ), 
+    validarCampos],
+    userPut);
+
+router.delete("/:id",[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existUserById ),
+    validarCampos
+], userDelete);
 
 module.exports = router;
