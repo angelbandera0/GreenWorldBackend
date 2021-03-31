0e8b50c4f1dfc58a64dfdf534c28d13eb960851b
jldelatorre <jorgedelatorredelarosa@gmail.com>
2021-03-31 12:18:14 +0200

    Añadido manejo de excepciones

--- controllers/plantaController.js
+++ controllers/plantaController.js
@@ -12,8 +12,8 @@ const plantaGet = async(req = request, res = response) => {
     ]);
 
     res.status(200).send({
-        "total":total,
-        "plantas":plantas
+        total: total,
+        plantas: plantas
     });
 }
 //Agregar Planta
@@ -21,9 +21,16 @@ const plantaPost= async(req,res=response)=>{
     const { name,  img ,description , cuidados ,isConMaceta, isPersonalizable} = req.body;
     const planta = new Planta({ name,  img ,description , cuidados ,isConMaceta, isPersonalizable});
 
-    // Guardar en BD
-    await planta.save();
-    res.status(201).send({"planta":planta});
+    try{
+        // Guardar en BD
+        await planta.save();
+        res.status(201).send({
+            planta: planta,
+            msg: "Planta creada correctame",
+        });
+    }catch(e){
+        res.status(400).send({ msg: "Ha ocurrido un error al adicionar" });
+    }
 }
 
 
@@ -33,19 +40,30 @@ const plantaPut= async(req,res=response)=>{
     const {id}=req.params;
     const { _id,...resto} = req.body;
 
-
-    //Buscar y actualizar
-    const planta = await Planta.findByIdAndUpdate( id, resto );
-    res.status(200).send({"planta":planta});
-    
+    try{
+        //Buscar y actualizar
+        const planta = await Planta.findByIdAndUpdate( id, resto );
+        res.status(200).send({
+            planta: planta,
+            msg: "Planta Actualizada Correctame"
+        });
+    }catch(e){
+        res.status(400).send({ msg: "Ha ocurrido un error en la actualizacón" });
+    }
 
 }
 
 //Eliminar Planta
 const plantaDelete= async(req,res=response)=>{
  const { id } = req.params;
-    //Fisicamente lo borramos
-     await Planta.findByIdAndRemove( id);
+    
+    try{
+        //Fisicamente lo borramos
+        await Planta.findByIdAndRemove( id);
+        res.status(200).send({ msg: "Planta eliminada correctamente" });
+    }catch(e){
+        res.status(400).send({ msg: "Ha ocurrido un error en la eliminación" });
+    }
     
 }
 
