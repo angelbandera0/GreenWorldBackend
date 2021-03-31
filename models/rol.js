24a6f15b0caac98003802518cb10fb70d69d1aaf
angel <angelbandera0@gmail.com>
2021-03-31 18:28:01 +0200

    se agrego el campo createdAt y updatedAt

--- models/rol.js
+++ models/rol.js
@@ -4,6 +4,14 @@ const RoleSchema = Schema({
     rol: {
         type: String,
         required: [true, 'El rol es obligatorio']
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now()
     }
 });
 
