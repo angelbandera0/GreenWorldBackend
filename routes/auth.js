e0ee7e2e9e966921d119ece04f903a122f56953d
angel <angelbandera0@gmail.com>
2021-04-05 20:30:42 +0200

    se inserstó una nueva ruta para hacer el login por google

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/');
-const { login } = require('../controllers/authController');
+const { login, googleSignin } = require('../controllers/authController');
 
 
 const router = Router();
@@ -12,6 +12,11 @@ router.post('/login',[
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ],login );
+
+router.post('/google',[
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignin );
 
 
 
